0b7ad362cd344dfaea55ef80c52d3648bd6219f9
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-19 12:48:50 +0530

    map.() function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Second from './Second';
 
 function App() {
-  function Mycombo(props){
-    return <h1>my combo {props.name}</h1>;
-  }
+  const theFruits = [
+    {id:1,name:"apple",color:"red"},
+    { id: 2,name: "pinapple", color:"yellow"},
+    { id: 3,name: "grapes", color:"green"},
+    { id: 4,name: "green apple", color:"green"},
+  ]
   return (
     <div>
-     <h1 className='visu'>heoo</h1>
-     <Mycombo name='sheddy' />
      <Second />
+     { theFruits.map((fruits)=>{
+      return <h2 key={fruits.id}>This is {fruits.name} with have color {fruits.color}</h2>
+     })}
     </div>
     
   );
