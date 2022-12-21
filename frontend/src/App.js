a5409b1de25f1c4072b792e0136c71c96eff3cd8
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-21 11:24:22 +0530

    components and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,21 @@
-// import logo from './logo.svg';
-import React from 'react';
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+import React from 'react'
 import './App.css';
-import Second from './Second';
-import { useState } from 'react';
+import Home from "./Components/Home";
+import Login from "./Components/Login"
+import Listing from "./Components/Listing"
+
+
 
 function App() {
-  const theFruits = [
-    {id:1,name:"apple",color:"red"},
-    { id: 2,name: "pinapple", color:"yellow"},
- ]
-  const [Count, setCount] = useState(0);
-  const [Name ,setName] = useState('vishnu');
   return (
-    <div>
-     <Second />
-     { theFruits.map((fruits)=>{
-      return <h2 key={fruits.id}>This is {fruits.name} with have color {fruits.color}</h2>
-     })}
-     <div>
-        <button onClick={()=> setCount(Count+1)} className='butt'>Add+</button>
-        <button onClick={()=> setCount(Count-1)} className="butt">Minus-</button>
-     </div>
-    <button onClick={()=> setName('')} className="butt">add name</button>
-     <h1 className='count'>Counter:{Count}</h1>
-     <h1>Name:{Name}</h1>
-     { Count > 10 ? <h1> more than 10</h1> : <h1>less than 10</h1>}
-     
-    </div>
-    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/listing" element={<Listing />}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <h1>This is login page</h1>
+    </div>
+  )
+}
+
+export default Login
