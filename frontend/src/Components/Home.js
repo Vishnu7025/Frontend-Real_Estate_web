dcac2b6590c8fe0897f80f5d65895987908b722a
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-21 11:56:22 +0530

    Links

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div>
         <h1>This is home page</h1>
+        <Link to='/Login'>login</Link>
+        <br />
+        <Link to='/listing'>listing</Link>
     </div>
   )
 }
