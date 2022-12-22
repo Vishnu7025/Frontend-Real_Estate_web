e72e646204cb05b321c3b990dd94ae6f0f03acf1
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-22 10:05:45 +0530

    useState with MUI

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 function Login() {
+  const [btnColor,setBtnColor] = useState('error');
+
   return (
     <div>
         <h1>This is login page</h1>
-        <Button variant='text'>hello</Button>
+        <Button onClick={() => setBtnColor("success")} color={btnColor} size='large' variant='contained'>hello</Button>
         
     </div>
   )
