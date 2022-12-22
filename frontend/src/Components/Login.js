96f662faf0fd0b4f6ef35e0d84101ad4232bfdcb
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-22 09:49:17 +0530

    Material UI

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Button from '@mui/material/Button';
 function Login() {
   return (
     <div>
         <h1>This is login page</h1>
+        <Button variant='text'>hello</Button>
+        
     </div>
   )
 }
