b4ba704ecefc91f45df31652cc48e8079a344b15
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-25 09:32:52 +0530

    Update Login.js

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -19,7 +19,7 @@ function Login() {
         <Typography align='center' variant='h2'>heooo</Typography>
       <Typography variant='body1'>
         What is Lorem Ipsum?
-        Lorem Ipsum is simy dummy text of the printting and typesetting industry. Lorem Ipsssum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop </Typography>
+        Lorem Ipsum is simmy dummy text of the printting and typesetting industry. Lorem Ipsssum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop </Typography>
         <Button onClick={() => setBtnColor("success")} color={btnColor} size='large' variant='contained'>hello</Button>
         
     </div>
