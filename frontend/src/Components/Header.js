bff67d105732403ad12330ad9dab874a2ad30b01
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-28 18:00:57 +0530

    front end

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -1,65 +1,196 @@
-import React, { useState } from 'react'
-import { Link, useNavigate} from 'react-router-dom'
- 
-//Mui imports
+import React, { useState, useContext, useEffect } from "react";
+import Axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
-import { Button, Typography, Grid, AppBar, Toolbar } from "@mui/material";
-import { makeStyles } from '@mui/styles'
+// MUI imports
+import {
+	Button,
+	Typography,
+	Grid,
+	AppBar,
+	Toolbar,
+	Menu,
+	MenuItem,
+	Snackbar,
+} from "@mui/material";
 
-// Componets
-import CustomCard from './CustomCard'
+// Contexts
+import StateContext from "../Contexts/StateContext";
+import DispatchContext from "../Contexts/DispatchContext";
 
-const useStyles = makeStyles({
-    leftNav: {
-        marginRight: "auto",
-    },
-    rightNav: {
-        marginLeft: "auto",
-        marginRight: "10rem",
-    },
-});
-const propertyBtn = {
-    backgroundColor: "green",
-    color: "white",
-    width: "15rem",
-    fontSize: "1.1rem",
-    marginRight: "1rem",
-
-}
-const loginBtn = {
-    backgroundColor: "white",
-    color: "black",
-    width: "15rem",
-    fontSize: "1.1rem",
-    marginleft: "1rem",
-}
+// Components
+import CustomCard from "./CustomCard";
 
 function Header() {
-    const classes = useStyles();
-    const navigate = useNavigate();
-  return (
-      <AppBar position="static" style={{ backgroundColor: "black" }}>
-          <Toolbar variant="dense">
-              <div className={classes.leftNav}>
-                  <Button color='inherit' onClick={() =>navigate('/')}>
-                      <Typography variant='h4'>sk</Typography>{" "}
-                  </Button>
-              </div>
-              <div>
-                  <Button color='inherit' style={{ mariginRight: "2rem" }} onClick={() => navigate('Listing')}>
-                      <Typography variant='h6'>Listing</Typography>{" "}
-                  </Button>
-                  <Button color='inherit' style={{ mariginLeft: "2rem" }}>
-                      <Typography variant='h6'>Agencies</Typography>{" "}
-                  </Button>
-              </div>
-              <div className={classes.rightNav}>
-                  <Button style={propertyBtn}>Add property</Button>
-                  <Button style={loginBtn} onClick={() => navigate('login')} >Login</Button>
-              </div>
-          </Toolbar>
-      </AppBar>
-  )
+	const navigate = useNavigate();
+	const GlobalState = useContext(StateContext);
+	const GlobalDispatch = useContext(DispatchContext);
+
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	function HandleProfile() {
+		setAnchorEl(null);
+		navigate("/profile");
+	}
+
+	const [openSnack, setOpenSnack] = useState(false);
+
+	async function HandleLogout() {
+		setAnchorEl(null);
+		const confirmLogout = window.confirm("Are you sure you want to leave?");
+		if (confirmLogout) {
+			try {
+				const response = await Axios.post(
+					"https://www.lbrepcourseapi.com/api-auth-djoser/token/logout/",
+					GlobalState.userToken,
+					{ headers: { Authorization: "Token ".concat(GlobalState.userToken) } }
+				);
+
+				GlobalDispatch({ type: "logout" });
+				setOpenSnack(true);
+			} catch (e) {}
+		}
+	}
+
+	useEffect(() => {
+		if (openSnack) {
+			setTimeout(() => {
+				navigate(0);
+			}, 1500);
+		}
+	}, [openSnack]);
+
+	return (
+		<AppBar position="static" style={{ backgroundColor: "black" }}>
+			<Toolbar>
+				<div style={{ marginRight: "auto" }}>
+					<Button color="inherit" onClick={() => navigate("/")}>
+						<Typography variant="h4">LBREP</Typography>{" "}
+					</Button>
+				</div>
+				<div>
+					<Button
+						color="inherit"
+						onClick={() => navigate("/listings")}
+						style={{ marginRight: "2rem" }}
+					>
+						<Typography variant="h6">Listings</Typography>{" "}
+					</Button>
+					<Button
+						color="inherit"
+						style={{ marginLeft: "2rem" }}
+						onClick={() => navigate("/agencies")}
+					>
+						{" "}
+						<Typography variant="h6">Agencies</Typography>{" "}
+					</Button>
+				</div>
+				<div style={{ marginLeft: "auto", marginRight: "10rem" }}>
+					<Button
+						onClick={() => navigate("/addproperty")}
+						style={{
+							backgroundColor: "green",
+							color: "white",
+							width: "15rem",
+							fontSize: "1.1rem",
+							// marginRight: "1rem",
+							// "&:hover": {
+							// 	backgroundColor: "blue",
+							// },
+						}}
+					>
+						Add Property
+					</Button>
+
+					{GlobalState.userIsLogged ? (
+						<Button
+							style={{
+								backgroundColor: "white",
+								color: "black",
+								width: "15rem",
+								fontSize: "1.1rem",
+								marginLeft: "1rem",
+								// "&:hover": {
+								// 	backgroundColor: "green",
+								// },
+							}}
+							onClick={handleClick}
+							// onClick={() => navigate("/login")}
+						>
+							{GlobalState.userUsername}
+						</Button>
+					) : (
+						<Button
+							style={{
+								backgroundColor: "white",
+								color: "black",
+								width: "15rem",
+								fontSize: "1.1rem",
+								marginLeft: "1rem",
+								// "&:hover": {
+								// 	backgroundColor: "green",
+								// },
+							}}
+							onClick={() => navigate("/login")}
+						>
+							Login
+						</Button>
+					)}
+
+					<Menu
+						id="basic-menu"
+						anchorEl={anchorEl}
+						open={open}
+						onClose={handleClose}
+						MenuListProps={{
+							"aria-labelledby": "basic-button",
+						}}
+					>
+						<MenuItem
+							style={{
+								color: "black",
+								backgroundColor: "green",
+								width: "15rem",
+								fontWeight: "bolder",
+								borderRadius: "15px",
+								marginBottom: "0.25rem",
+							}}
+							onClick={HandleProfile}
+						>
+							Profile
+						</MenuItem>
+						<MenuItem
+							style={{
+								color: "black",
+								backgroundColor: "red",
+								width: "15rem",
+								fontWeight: "bolder",
+								borderRadius: "15px",
+							}}
+							onClick={HandleLogout}
+						>
+							Logout
+						</MenuItem>
+					</Menu>
+					<Snackbar
+						open={openSnack}
+						message="You have successfully logged out!"
+						anchorOrigin={{
+							vertical: "bottom",
+							horizontal: "center",
+						}}
+					/>
+				</div>
+			</Toolbar>
+		</AppBar>
+	);
 }
 
-export default Header
+export default Header;
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,54 +1,55 @@
-import React,{useState} from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-//Mui imports
-import { Button, Typography,Grid ,AppBar,Toolbar} from "@mui/material";
-import {makeStyles} from '@mui/styles'
+// MUI imports
+import { Button, Typography, Grid, AppBar, Toolbar } from "@mui/material";
 
-//components
-import Header from './Header';
-import CustomCard from './CustomCard'
+// Components
+import CustomCard from "./CustomCard";
+import Header from "./Header";
 
-//Assets
-import city  from './Assets/city.webp';
-
-const useStyles = makeStyles({
-  cityImg:{
-    width:'100%',
-    height:'92vh'
-  },
-  overlayText:{
-    position:'absolute',
-    zIndex:'100',
-    top:'100px',
-    textAlign:'center', 
-  },
-  homeText:{
-    color:'grey',
-    fontWeight:'bolder',
-  },
-  homeBtn:{
-    fontSize:'3.5rem',
-    borderRadius:'15px',
-    backgroundColor:'green',
-    marginTop:'2rem',
-    boxShadow:'3px 3px 3px white'
-  }
-});
+// Assets
+import city from "./Assets/city.jpg";
 
 function Home() {
-  const [btnColor, setBtnColor] = useState("error");
-  const classes = useStyles();
-  return (
-  <> 
-      <img src='{city}' className={classes.cityImg} />
-      <div className={classes.overlayText}>
-        <Typography variant='h1' className={classes.homeText}>FIND YOUR <span style={{color:"green"}}> NEXT PROPERTY</span> ON SK WEBSITE </Typography>
-        <Button variant='conatined' className={classes.homeBtn}>SEE ALL PROPERTIES</Button>
-      </div>
-  </>
+	const [btnColor, setBtnColor] = useState("error");
 
-  )
+	return (
+		<>
+			<div style={{ position: "relative" }}>
+				<img src={city} style={{ width: "100%", height: "92vh" }} />
+				<div
+					style={{
+						position: "absolute",
+						zIndex: "100",
+						top: "100px",
+						left: "20px",
+						textAlign: "center",
+					}}
+				>
+					<Typography
+						variant="h1"
+						style={{ color: "white", fontWeight: "bolder" }}
+					>
+						FIND YOUR <span style={{ color: "green" }}>NEXT PROPERTY</span> ON
+						THE LBREP WEBSITE
+					</Typography>
+					<Button
+						variant="contained"
+						style={{
+							fontSize: "3.5rem",
+							borderRadius: "15px",
+							backgroundColor: "green",
+							marginTop: "2rem",
+							boxShadow: "3px 3px 3px white",
+						}}
+					>
+						SEE ALL PROPERTIES
+					</Button>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default Home
+export default Home;
