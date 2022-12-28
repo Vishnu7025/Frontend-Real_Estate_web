5ee0bd716275a6b30c758e53036efacc1dc3332d
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-28 10:55:07 +0530

    navbar

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,24 +1,67 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import CustomCard from './CustomCard'
-import { Button, Typography,Grid } from "@mui/material";
-import { ScopedCssBaseline } from '@mui/material';
+import { Button, Typography,Grid ,AppBar,Toolbar} from "@mui/material";
+import CssBaseline from '@mui/material/CssBaseline';
+import {makeStyles} from '@mui/styles'
+
+const useStyles = makeStyles({
+  leftNav:{
+    marginRight:"auto",
+  },
+  rightNav:{
+    marginLeft:"auto",
+    marginRight:"10rem",
+  },
+  propertyBtn: {
+    backgroundColor:"green",
+    color:"white",
+    width:"15rem",
+    fontSize:"1.1rem",
+    marginRight:"1rem",
+    '&:hover': {
+      backgroundColor: "blue",
+    }
+  },
+  loignBtn: {
+    backgroundColor: "white",
+    color: "black",
+    width: "15rem",
+    fontSize: "1.1rem",
+    marginleft: "1rem",
+    '&:hover':{
+      backgroundColor:"green",
+    }
+  },
+});
 
 function Home() {
+  const [btnColor, setBtnColor] = useState("error");
+  const classes = useStyles();
   return (
   <>
-        <Grid item xs={8}>
-          <CustomCard />
-        </Grid>
-        <Grid item xs={4}>
-              <CustomCard />
-                </Grid>
-        <Grid item xs={4}>
-              <CustomCard />
-                </Grid>
-        <Grid item xs={8}>
-              <CustomCard />
-                </Grid>
+    <CssBaseline />
+      <AppBar position="static" style={{backgroundColor:"black"}}>
+        <Toolbar variant="dense">
+          <div className={classes.leftNav}>
+            <Button color='inherit'>
+              <Typography variant='h4'>sk</Typography>{" "}
+              </Button>
+          </div>
+          <div>
+            <Button color='inherit' style={{mariginRight:"2rem"}}>
+              <Typography variant='h6'>Listing</Typography>{" "}
+              </Button>
+            <Button color='inherit' style={{ mariginLeft: "2rem" }}>
+              <Typography variant='h6'>Agencies</Typography>{" "} 
+              </Button>
+          </div>
+          <div className={classes.rightNav}>
+            <Button className={classes.propertyBtn}>Add Property</Button>
+            <Button className={classes.loignBtn}>Login</Button>
+          </div>
+        </Toolbar>
+      </AppBar>
   </>
 
   )
